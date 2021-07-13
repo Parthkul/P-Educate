c3a2544bf638f66dd4a0be66afd9893412e0b2f5
Parth Kulshreshtha <85359173+Parthkul@users.noreply.github.com>
2021-07-13 11:14:25 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -6,7 +6,6 @@ rightnav = document.querySelector('.right-nav')
 
 burger.addEventListener('click', () => {
     navbar.classList.toggle('h-nav')
-    navbar.classList.toggle('sticky-nav')
     rightnav.classList.toggle('v-class')
     navList.classList.toggle('v-class')
 })
